6e64b4ad022ddb1dc0941ca9b1b05d2ea6cf8657
Mxm_msv <oikioikioikiabrams@gmail.com>
2026-03-18 01:26:18 +0300

    feat: add env-based start scripts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { buildServer } from "@/server.js";
+import "dotenv/config";
 
 const app = buildServer();
 
 try {
-  await app.listen({ port: 3000 });
+  const port = Number(process.env.PORT);
+
+  if (Number.isNaN(port)) {
+    throw new Error("PORT environment variable must be a valid number");
+  }
+
+  await app.listen({ port });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
